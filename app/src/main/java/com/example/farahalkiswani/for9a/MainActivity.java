836dac0c228c9872ab74daf627bd0e4fb684a86c
Farah alkiswani <farahalkiswani196@gmail.com>
2018-07-22 16:20:44 +0300

    For9a

--- app/src/main/java/com/example/farahalkiswani/for9a/MainActivity.java
+++ app/src/main/java/com/example/farahalkiswani/for9a/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
    private Button Logout;
 
 
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
