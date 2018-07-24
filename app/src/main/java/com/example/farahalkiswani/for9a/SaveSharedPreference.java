c496f5f6203df446aacb8c52c4725c0e9a7be2a5
Farah Al-kiswani <farahalkiswani196@gmail.com>
2018-07-24 12:49:21 +0300

    after

--- app/src/main/java/com/example/farahalkiswani/for9a/SaveSharedPreference.java
+++ app/src/main/java/com/example/farahalkiswani/for9a/SaveSharedPreference.java
@@ -3,6 +3,7 @@ package com.example.farahalkiswani.for9a;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,18 +22,22 @@ public class SaveSharedPreference {
 
     public static void setLoggedIn(Context context, String userInfo) {
         SharedPreferences.Editor editor = getPreferences( context ).edit();
-        editor.putBoolean( LOGGED_IN_PREF, true );
+
         try {
             JSONObject obj = new JSONObject( userInfo );
             editor.putInt( "id", obj.getInt( "id" ) );
             editor.putString( "first_name", obj.getString("first_name"));
+
             editor.putString( "last_name", obj.getString( "last_name" ) );
             editor.putString( "email", obj.getString( "email" ) );
             editor.putInt( "status", obj.getInt( "status" ) );
             editor.putString( "pic", obj.getString( "pic" ) );
             editor.putInt( "show_steps", obj.getInt( "show_steps" ) );
             editor.putString("gender", obj.getString( "gender" ) );
-            editor.apply();
+           editor.apply();
+Log.d("first", obj.getString("first_name"));
+
+
 
         } catch (JSONException e)
         {
@@ -40,13 +45,12 @@ public class SaveSharedPreference {
         }
 
 
-        editor.apply();
     }
 
 
     static boolean getLoggedStatus(Context context) {
 
-     return   PreferenceManager.getDefaultSharedPreferences(context).getAll().containsKey( LOGGED_IN_PREF );
+     return   PreferenceManager.getDefaultSharedPreferences(context).getAll().containsKey( "userInfo" );
 
     }
 
