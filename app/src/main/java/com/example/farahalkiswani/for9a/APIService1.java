7a8538682fb5cf9c09e3bb88d198208314621b42
Farah Al-kiswani <farahalkiswani196@gmail.com>
2018-07-25 15:24:49 +0300

    add drawer1

--- app/src/main/java/com/example/farahalkiswani/for9a/APIService1.java
+++ app/src/main/java/com/example/farahalkiswani/for9a/APIService1.java
@@ -3,11 +3,15 @@ package com.example.farahalkiswani.for9a;
 import com.google.gson.JsonObject;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -23,8 +27,15 @@ public interface APIService1 {
     @POST("/user/forget-password")
     Call<JsonObject> userChangePassword(@Field("email") String email);
 
-    @Headers("authentication: MNrAG2QLv41sWs2qd-spRKT594bfwROM")
     @GET("/user/timeline")
-    Call<JsonObject> getTimeline(@Query("count") Integer count, @Query("since_id") Integer since_id);
+    Call<JsonObject> getTimeline(@Header("authentication") String auth, @Query("count") Integer count, @Query("since_id") Integer since_id);
+
+
+    @PUT("/opportunity/{id}/pin")
+    Call<JsonObject> Pin(@Header("authentication") String auth, @Path("id") Integer id);
+
+
+    @DELETE("/opportunity/{id}/un-pin")
+    Call<JsonObject> DeletPin(@Header("authentication") String auth, @Path("id") Integer id);
 
 }
